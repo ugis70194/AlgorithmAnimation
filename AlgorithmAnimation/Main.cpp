63be1eab60f7135a829a1e4d20fae0016137b606
ugis <ugis70194@yahoo.co.jp>
2019-04-17 21:15:35 +0900

    add margeSort

--- AlgorithmAnimation/Main.cpp
+++ AlgorithmAnimation/Main.cpp
@@ -1,98 +1,162 @@
 ﻿# include <Siv3D.hpp> // OpenSiv3D v0.3.2
 
-void updateTexture(Image& image, DynamicTexture& texture) {
-	texture.fill(image);
-	texture.draw();
-	System::Update();
-}
+class Sort {
+private:
+	Image m_image;
+	Array<int32> m_arr;
+	DynamicTexture m_texture;
 
-void repaintImage(Image& image, Array<int32>& arr, const int32& repaintIndex) {
-	int32 x = repaintIndex * 2;
-	for (int32 y = 0; y < 600; y++) {
-		image[y][x] = Color(0, 0, 0);
-		image[y][x + 1] = Color(0, 0, 0);
+	void updateTexture() {
+		m_texture.fill(m_image);
+		m_texture.draw();
+		System::Update();
 	}
-	for (int32 y = 600 - arr[repaintIndex]; y < 600; y++) {
-		image[y][x] = Color(255, 255, 255);
-		image[y][x + 1] = Color(255, 255, 255);
-	}
-}
 
-void updateImage(Image& image, DynamicTexture& texture, Array<int32>& arr, const int32& swapedIndexLeft, const int32& swapedIndexRight) {
-	repaintImage(image, arr, swapedIndexLeft);
-	repaintImage(image, arr, swapedIndexRight);
-	updateTexture(image, texture);
-}
-
-void paintPivot(Image& image, DynamicTexture& texture, Array<int32>& arr, int32 pivotIndex) {
-	int32 x = pivotIndex * 2;
-	for (int32 y = 600 - arr[pivotIndex]; y < 600; y++) {
-		image[y][x] = Color(255, 0, 0);
-		image[y][x + 1] = Color(255, 0, 0);
-	}
-}
-
-int32 partition(Array<int32>& arr, const int32& left, const int32& right, Image& image, DynamicTexture& texture) {
-	const int32 pivot = arr[right];
-	paintPivot(image, texture, arr, right);
-
-	int32 pivotIndex = left - 1;
-	for (int i = left; i < right; i++) {
-		if (arr[i] <= pivot) {
-			pivotIndex++;
-			std::swap(arr[i], arr[pivotIndex]);
-
-			updateImage(image, texture, arr, i, pivotIndex);
+	void repaintImage(const int32& repaintIndex) {
+		int32 x = repaintIndex * 2;
+		for (int32 y = 0; y < 600; y++) {
+			m_image[y][x] = Color(0, 0, 0);
+			m_image[y][x + 1] = Color(0, 0, 0);
+		}
+		for (int32 y = 600 - m_arr[repaintIndex]; y < 600; y++) {
+			m_image[y][x] = Color(255, 255, 255);
+			m_image[y][x + 1] = Color(255, 255, 255);
 		}
 	}
-	std::swap(arr[right], arr[++pivotIndex]);
-	updateImage(image, texture, arr, right, pivotIndex);
 
-	return pivotIndex;
-}
-
-void quickSort(Array<int32>& arr, const int32& left, const int32& right, Image& image, DynamicTexture& texture) {
-	if (left >= right) return;
-
-	int32 pivotIndex = partition(arr, left, right, image, texture);
-
-	quickSort(arr, left, pivotIndex - 1, image, texture);
-	quickSort(arr, pivotIndex + 1, right, image, texture);
-}
-
-
-void init(Image & image, DynamicTexture texture, Array<int32> & arr) {
-	for (int32 k = 0; k < 400; k++) arr[k] = Random<int32>(100, 600);
-
-	image.fill(Palette::Black);
-	int32 w = 0;
-	for (int32 x = 0; x < 400; x++) {
-		for (int32 y = 600 - arr[x]; y < 600; y++) {
-			image[y][w] = Color(255, 255, 255);
-			image[y][w + 1] = Color(255, 255, 255);
-		}
-		w += 2;
+	void updateImage(const int32& swapedIndexLeft, const int32& swapedIndexRight) {
+		repaintImage(swapedIndexLeft);
+		repaintImage(swapedIndexRight);
+		updateTexture();
 	}
-	updateTexture(image, texture);
-}
 
+	void paintPivot(int32 pivotIndex) {
+		int32 x = pivotIndex * 2;
+		for (int32 y = 600 - m_arr[pivotIndex]; y < 600; y++) {
+			m_image[y][x] = Color(255, 0, 0);
+			m_image[y][x + 1] = Color(255, 0, 0);
+		}
+	}
+
+public:
+	Sort()
+		: m_image(800, 600)
+		, m_texture(m_image)
+		, m_arr(400)
+	{
+		init();
+	}
+
+	void init() {
+		for (int32 k = 0; k < 400; k++) m_arr[k] = Random<int32>(100, 600);
+
+		m_image.fill(Palette::Black);
+		int32 w = 0;
+		for (int32 x = 0; x < 400; x++) {
+			for (int32 y = 600 - m_arr[x]; y < 600; y++) {
+				m_image[y][w] = Color(255, 255, 255);
+				m_image[y][w + 1] = Color(255, 255, 255);
+			}
+			w += 2;
+		}
+		updateTexture();
+	}
+
+	int32 partition(const int32& left, const int32& right) {
+		const int32 pivot = m_arr[right];
+		paintPivot(right);
+
+		int32 pivotIndex = left - 1;
+		for (int i = left; i < right; i++) {
+			if (m_arr[i] <= pivot) {
+				pivotIndex++;
+				std::swap(m_arr[i], m_arr[pivotIndex]);
+
+				updateImage(i, pivotIndex);
+			}
+		}
+		std::swap(m_arr[right], m_arr[++pivotIndex]);
+		updateImage(right, pivotIndex);
+
+		return pivotIndex;
+	}
+
+
+	void quickSort(const int32& left, const int32& right) {
+		if (left >= right) return;
+
+		int32 pivotIndex = partition(left, right);
+
+		quickSort(left, pivotIndex - 1);
+		quickSort(pivotIndex + 1, right);
+	}
+
+	void merge(const int32& left, const int32& mid, const int32& right) {
+		const int32 leftSideLength = mid - left;
+		const int32 rightSideLength = right - mid;
+
+		Array<int32> leftSide(leftSideLength);
+		Array<int32> rightSide(rightSideLength);
+
+		for (int k = 0; k < leftSideLength; k++) {
+			leftSide[k] = m_arr[left + k];
+		}
+		for (int k = 0; k < rightSideLength; k++) {
+			rightSide[k] = m_arr[mid + k];
+		}
+
+		int l = 0, r = 0;
+		for (int k = left; k < right; k++) {
+			if (l >= leftSideLength) {
+				m_arr[k] = rightSide[r];
+				r++;
+				continue;
+			}
+			if (r >= rightSideLength) {
+				m_arr[k] = leftSide[l];
+				l++;
+				continue;
+			}
+
+			if (leftSide[l] <= rightSide[r]) {
+				m_arr[k] = leftSide[l];
+				l++;
+			}
+			else {
+				m_arr[k] = rightSide[r];
+				r++;
+			}
+			repaintImage(k);
+			updateTexture();
+		}
+	}
+
+	void mergeSort(const int32& left, const int32& right) {
+		if (right - left > 1) {
+			const int32 mid = (left + right) / 2;
+			mergeSort(left, mid);
+			mergeSort(mid, right);
+			merge(left, mid, right);
+		}
+	}
+};
 
 void Main()
 {
+	constexpr int32 elementCount = 400;
+	Sort quickSort;
+	Sort margeSort;
 
-	DynamicTexture texture;
-	Image image(800, 600);
-	Array<int32> arr(400);
 
 	while (System::Update())
 	{
 		if (KeyR.down())
 		{
-			init(image, texture, arr);
-			quickSort(arr, 0, 400 - 1, image, texture);
+			//quickSort.init();
+			//quickSort.quickSort(0, elementCount - 1);
+			margeSort.init();
+			margeSort.mergeSort(0, elementCount);
 		}
-		texture.fill(image);
-		texture.draw();
 
 	}
 }
